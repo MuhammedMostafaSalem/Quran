85cd63024f53b3b0c7573f49c05ffb5386f8a7d7
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2024-03-21 10:07:53 +0200

    Add theme container and screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,23 @@ export default withMT({
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    container: {
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+    },
+    screens: {
+      'sm': '576px',
+      'md': '768px',
+      'lg': '992px',
+      'xl': '1200px',
+      '2xl': '1400px',
+      "sm-max": { max: "575px" },
+    },
     extend: {},
   },
   plugins: [],
