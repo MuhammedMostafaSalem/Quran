4570c7df77173c735678174022c8deae3c9c4486
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2024-03-22 08:21:29 +0200

    Create Banner component

---
+++ src/components/Banner.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Banner  = () => {
+    return (
+        <div className="banner">
+            <div className="overlay"></div>
+            <div className="banner-text">
+                <h6 className='text-[10px] sm:text-[15px] md:text-[18px] uppercase font-[700] tracking-[2.5px]'>A pleasant journey in the realms of the</h6>
+                <h2 className='text-[30px] sm:text-[40px] md:text-[45px] font-[700] uppercase mt-[15px] mb-[25px]'><span className='text-[#22b3c1]'>Quran</span> Karem</h2>
+            </div>
+        </div>
+    )
+}
+
+export default Banner 
