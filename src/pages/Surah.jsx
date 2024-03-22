768530a3a45fef466e0b2195f7951569d6b8fa83
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2024-03-22 11:02:34 +0200

    Add Audio in Surah page

--- src/pages/Surah.jsx
+++ src/pages/Surah.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Banner from '../components/Banner'
 import SearchForm from '../components/SearchForm'
+import Audio from '../components/Audio'
 
 const Surah = () => {
     return (
         <div>
             <Banner />
             <SearchForm />
+            <Audio />
         </div>
     )
 }
