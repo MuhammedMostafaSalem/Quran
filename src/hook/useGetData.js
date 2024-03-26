8473146ebd99df425b49f943e6c515d1a47e70ad
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2024-03-26 14:28:57 +0200

    Create useGetData

---
+++ src/hook/useGetData.js
@@ -0,0 +1,9 @@
+import baseURL from '../api/baseURL'
+
+const useGetData = async(url, params) => {
+    const res = await baseURL.get(url, params);
+
+    return res;
+}
+
+export default useGetData
