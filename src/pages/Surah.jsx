8c89d3f7e76d7bfdad63d2ebd2a13ff9cfcdd41e
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2024-03-26 14:28:19 +0200

    Add cutom hook (useGetMoshaf)

--- src/pages/Surah.jsx
+++ src/pages/Surah.jsx
@@ -2,13 +2,37 @@ import React from 'react'
 import Banner from '../components/Banner'
 import SearchForm from '../components/SearchForm'
 import Audio from '../components/Audio'
+import useGetMoshaf from '../hooks/useGetMoshaf'
 
 const Surah = () => {
+    const [
+        selectedOption,
+        selectedSurah,
+        nameData,
+        handleSelectChange,
+        handleClearData,
+        surahData,
+        server,
+        onClickSurah
+    ] = useGetMoshaf();
+
     return (
         <div>
             <Banner />
-            <SearchForm />
-            <Audio />
+            <SearchForm
+                selectedOption={selectedOption}
+                nameData={nameData}
+                handleSelectChange={handleSelectChange}
+                handleClearData={handleClearData}
+                surahData={surahData}
+                server={server}
+                onClickSurah={onClickSurah}
+            />
+            {
+                selectedSurah ?
+                    <Audio selectedSurah={selectedSurah} />
+                : null
+            }
         </div>
     )
 }
