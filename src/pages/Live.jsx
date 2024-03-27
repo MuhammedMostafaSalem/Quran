2a6f69d971e58ddf57fee612045b573be9cc0957
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2024-03-27 12:34:20 +0200

    Create Live page

---
+++ src/pages/Live.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import BannerLIve from '../components/BannerLIve'
+import TabsLive from '../components/TabsLive'
+
+const Live = () => {
+    return (
+        <div>
+            <BannerLIve />
+            <TabsLive />
+        </div>
+    )
+}
+
+export default Live
