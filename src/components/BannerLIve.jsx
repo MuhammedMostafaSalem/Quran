4ece7bead499bdf85697505fe8af28fad9551474
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2024-03-27 12:32:57 +0200

    Create BannerLIve

---
+++ src/components/BannerLIve.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const BannerLIve = () => {
+    return (
+        <div className="bannerLive relative top-[70px] sm:top-[60px] md:top-[70px]">
+            <div className="banner-text">
+                <h6 className='text-[30px] sm:text-[35px] md:text-[48px] font-[700] font-[cursive] tracking-[2.5px]'>Live</h6>
+            </div>
+        </div>
+    )
+}
+
+export default BannerLIve
