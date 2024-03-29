414e923801773c7deaad91bb09e4ba3ba6536365
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2024-03-29 07:07:27 +0200

    Craete custom hook (useGetLive)

---
+++ src/hooks/useGetLive.js
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { getAllLive } from '../store/Actions/livTvAction';
+
+const useGetLive = () => {
+    const dispatch = useDispatch();
+    const {livTv} = useSelector(state => state.livTv);
+    const [activeTab, setActiveTab] = useState(0);
+
+    const handleTabClick = (value) => {
+        setActiveTab(value);
+    };
+
+    let liveData = []
+    try {
+        if(livTv.livetv) {
+            liveData = livTv.livetv
+        } else {
+            liveData =[]
+        }
+    } catch(err) {}
+    console.log(liveData)
+
+    useEffect(() => {
+        dispatch(getAllLive())
+    }, [])
+
+    return [
+        liveData,
+        activeTab,
+        handleTabClick,
+    ]
+}
+
+export default useGetLive
