ebe8a3067137c6bcaff889991e2ce87370778c4b
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2024-03-29 08:56:44 +0200

    Create Tafsir page

---
+++ src/pages/Tafsir.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import TafsirList from '../components/TafsirList'
+
+const Tafsir = () => {
+    return (
+        <div className='mt-[70px] sm:mt-[60px] md:mt-[70px]'>
+            <div className='container mx-auto py-[30px]'>
+                <h1 className="beautiful-title text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold text-center text-gray-400">الخلاصة من تفسير الطبري</h1>
+                <div className='w-full
+                    h-auto
+                    flex
+                    justify-center
+                    gap-4
+                    items-start
+                    flex-wrap
+                    overflow-y-auto
+                    mt-[30px]'
+                >
+                    <TafsirList />
+                    <TafsirList />
+                    <TafsirList />
+                    <TafsirList />
+                    <TafsirList />
+                    <TafsirList />
+                    <TafsirList />
+                    <TafsirList />
+                    <TafsirList />
+                    <TafsirList />
+                    <TafsirList />
+                    <TafsirList />
+                    <TafsirList />
+                    <TafsirList />
+                    <TafsirList />
+                    <TafsirList />
+                    <TafsirList />
+                    <TafsirList />
+                    <TafsirList />
+                    <TafsirList />
+                    <TafsirList />
+                    <TafsirList />
+                    <TafsirList />
+                    <TafsirList />
+                    <TafsirList />
+                    <TafsirList />
+                    <TafsirList />
+                    <TafsirList />
+                    <TafsirList />
+                    <TafsirList />
+                    <TafsirList />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Tafsir
