a2c4087caa0012bd565569c01dd2580d3016a441
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2024-03-29 08:58:08 +0200

    Add location in Footer component

--- src/components/utils/Footer.jsx
+++ src/components/utils/Footer.jsx
@@ -1,10 +1,18 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { useLocation } from 'react-router-dom';
 
 const Footer = () => {
+    const location = useLocation();
+    const [isTafsirPath, setIsTafsirPath] = useState(false);
     const [year, setYear] = useState(new Date().getFullYear());
 
+    useEffect(() => {
+        // setIsTafsirPath(location.pathname === '/tafsir');
+        setIsTafsirPath(location.pathname === "/tafsir")
+    }, [location.pathname]);
+
     return (
-        <footer className='bg-[#22b3c1] text-[#fff] mt-[50px]'>
+        <footer className={`bg-[#22b3c1] text-[#fff] ${isTafsirPath ? '' : 'mt-[50px]'}`}>
             <p className="font-bold text-base capitalize text-center py-[20px]">
                 &copy; {year} created with MuhammedMostafaSalem
             </p>
