9754276dee96b07b409eb17fc876820495040779
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2024-03-29 08:57:14 +0200

    Craete TafsirList component

---
+++ src/components/TafsirList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TafsirList = () => {
+    return (
+        <div className='text-center text-[#7e7d7d] px-[20px] py-[20px] sm-max:w-full w-[150px] rounded-[10px] border-[##7e7d7d] border-2'>
+            <p>سورة البقرة الايات من 1 الى 25</p>
+        </div>
+    )
+}
+
+export default TafsirList
