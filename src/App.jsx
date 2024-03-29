77ac1ebfef2360e06dfeca1f8149388e62238487
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2024-03-29 08:58:32 +0200

    Add Route (Tafsir page)

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from "./components/utils/Header"
 import Live from "./pages/Live";
 import Surah from "./pages/Surah"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Tafsir from "./pages/Tafsir";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route index element={<Surah />} />
         <Route path="/live" element={<Live />} />
+        <Route path="/tafsir" element={<Tafsir />} />
       </Routes>
       <Footer />
     </BrowserRouter>
