cd1a35f338698bb483350c42f27215ed000e1bc3
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2024-03-29 07:06:51 +0200

    Add livTv in store

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import recitersReducer from "./Reducers/recitersReducer";
+import livTvReducer from "./Reducers/livTvReducer";
 
 export const store = configureStore({
     reducer: {
         reciters: recitersReducer,
+        livTv: livTvReducer
     },
     devTools: true,
 })
