b66eafe3034e1804e6b83adab726093077b0bc79
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2024-03-29 09:37:03 +0200

    Add props item and onClickSurah

--- src/components/TafsirList.jsx
+++ src/components/TafsirList.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 
-const TafsirList = () => {
+const TafsirList = ({item, onClickSurah}) => {
     return (
-        <div className='text-center text-[#7e7d7d] px-[20px] py-[20px] sm-max:w-full w-[150px] rounded-[10px] border-[##7e7d7d] border-2'>
-            <p>سورة البقرة الايات من 1 الى 25</p>
+        <div className='text-center
+            text-[#7e7d7d]
+            px-[20px]
+            py-[20px]
+            sm-max:w-full
+            w-[150px]
+            min-h-[100px]
+            rounded-[10px]
+            border-[##7e7d7d]
+            border-2
+            flex
+            items-center
+            cursor-pointer'
+            onClick={() => onClickSurah(item.url)}
+        >
+            {item.name}
         </div>
     )
 }
