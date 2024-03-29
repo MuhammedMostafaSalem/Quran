0dfedba9ba9dc2a2186fc7575a36d8b748eee09f
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2024-03-29 09:30:06 +0200

    Add tafsir in store

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import recitersReducer from "./Reducers/recitersReducer";
 import livTvReducer from "./Reducers/livTvReducer";
+import tafasirReducer from "./Reducers/tafasirReducer";
 
 export const store = configureStore({
     reducer: {
         reciters: recitersReducer,
-        livTv: livTvReducer
+        livTv: livTvReducer,
+        tafsir: tafasirReducer
     },
     devTools: true,
 })
