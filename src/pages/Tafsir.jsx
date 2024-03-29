dfc02a05690705d86038c26b7c6e077b1f7ac884
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2024-03-29 09:36:37 +0200

    Add useGetTafsir in Tafsir page

--- src/pages/Tafsir.jsx
+++ src/pages/Tafsir.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import TafsirList from '../components/TafsirList'
+import Audio from '../components/Audio';
+import useGetTafsir from '../hooks/useGetTafsir';
 
 const Tafsir = () => {
+    const [
+        tafasirData,
+        soarData,
+        selectedSurah,
+        onClickSurah,
+    ] = useGetTafsir();
+    
     return (
         <div className='mt-[70px] sm:mt-[60px] md:mt-[70px]'>
             <div className='container mx-auto py-[30px]'>
-                <h1 className="beautiful-title text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold text-center text-gray-400">الخلاصة من تفسير الطبري</h1>
+                <h1 className="beautiful-title text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold text-center text-gray-400">{tafasirData.name}</h1>
                 <div className='w-full
                     h-auto
                     flex
@@ -16,39 +25,18 @@ const Tafsir = () => {
                     overflow-y-auto
                     mt-[30px]'
                 >
-                    <TafsirList />
-                    <TafsirList />
-                    <TafsirList />
-                    <TafsirList />
-                    <TafsirList />
-                    <TafsirList />
-                    <TafsirList />
-                    <TafsirList />
-                    <TafsirList />
-                    <TafsirList />
-                    <TafsirList />
-                    <TafsirList />
-                    <TafsirList />
-                    <TafsirList />
-                    <TafsirList />
-                    <TafsirList />
-                    <TafsirList />
-                    <TafsirList />
-                    <TafsirList />
-                    <TafsirList />
-                    <TafsirList />
-                    <TafsirList />
-                    <TafsirList />
-                    <TafsirList />
-                    <TafsirList />
-                    <TafsirList />
-                    <TafsirList />
-                    <TafsirList />
-                    <TafsirList />
-                    <TafsirList />
-                    <TafsirList />
+                    {
+                        soarData.map((item, index) => (
+                            <TafsirList key={index} item={item} onClickSurah={onClickSurah} />
+                        ))
+                    }
                 </div>
             </div>
+            {
+                selectedSurah ?
+                    <Audio selectedSurah={selectedSurah} />
+                : null
+            }
         </div>
     )
 }
