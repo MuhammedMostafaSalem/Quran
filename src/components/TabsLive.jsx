3ccc8d07845597630f1bbb1d7f0e0eabb29a464a
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2024-03-29 07:07:57 +0200

    Add useGetLive in TabsLive component

--- src/components/TabsLive.jsx
+++ src/components/TabsLive.jsx
@@ -1,81 +1,37 @@
-import React, { useState } from 'react'
-import {
-    Tabs,
-    TabsHeader,
-    TabsBody,
-    Tab,
-    TabPanel,
-} from "@material-tailwind/react";
+import React from 'react'
+import ReactPlayer from 'react-player';
+import useGetLive from '../hooks/useGetLive';
 
 const TabsLive = () => {
-    const [activeTab, setActiveTab] = useState('html');
-
-    const data = [
-        {
-            label: "HTML",
-            value: "html",
-            desc: `It really matters and then like it really doesn't matter.
-            What matters is the people who are sparked by it. And the people 
-            who are like offended by it, it doesn't matter.`,
-        },
-        {
-            label: "React",
-            value: "react",
-            desc: `Because it's about motivating the doers. Because I'm here
-            to follow my dreams and inspire other people to follow their dreams, too.`,
-        },
-    ]
-
-    const handleTabClick = (value) => {
-        setActiveTab(value); // Update active tab state on click
-    };
-
+    const [
+        liveData,
+        activeTab,
+        handleTabClick,
+    ] = useGetLive();
+    
     return (
-        <div className='container mx-auto mt-[100px]'>
-            <Tabs value={activeTab}>
-                <TabsHeader>
-                    {data.map(({ label, value }) => (
-                    <Tab key={value} value={value}>
-                        {label}
-                    </Tab>
-                    ))}
-                </TabsHeader>
-                <TabsBody>
-                    {data.map(({ value, desc }) => (
-                    <TabPanel key={value} value={value}>
-                        {desc}
-                    </TabPanel>
-                    ))}
-                </TabsBody>
-                </Tabs>
+        <div className="flex flex-col container mx-auto mt-[100px]">
+            <div className="flex mb-4 gap-5">
+                {liveData.map((tab, index) => (
+                    <button
+                        key={index}
+                        className={`w-full py-2 px-4 sm-max:text-[15px] rounded-[20px] transition-colors duration-300 ${
+                            activeTab === index ? 'text-[#fff] bg-[#22b3c1]' : 'text-gray-600 sm:hover:bg-gray-200 sm-max:focus:bg-gray-200 hover:text-gray-800'
+                        }`}
+                        onClick={() => handleTabClick(index)}
+                    >
+                        {tab.name}
+                    </button>
+                ))}
+            </div>
+            <div className='flex justify-center'>
+                {liveData.map((tab, index) => (
+                    <div key={index} className={`${activeTab === index ? '' : 'hidden'}`}>
+                        <ReactPlayer url={tab.url.replace('https://www.youtube.com/live/', 'https://www.youtube.com/embed/')} controls={true} className="react-player-custom" width='100%' height='100%' />
+                    </div>
+                ))}
+            </div>
         </div>
-        // <Tabs value={activeTab} className='container mx-auto mt-[100px]'>
-        //     <div className="flex gap-4 bg-transparent">
-        //         {data.map(({ label, value }) => (
-        //             <TabsHeader 
-        //                 key={value} 
-        //                 className={`cursor-pointer w-full rounded-[10px] text-center py-2 px-4 transition duration-300 ${activeTab === value ? 'bg-[#22b3c1] text-[#fff]' : 'text-[#22b3c1]'}`}
-        //                 onClick={() => handleTabClick(value)}
-        //             >
-        //                 {label}
-        //             </TabsHeader>
-        //         ))}
-        //     </div>
-        //     {/* <div className="mt-4">
-        //         {data.map(({ value, desc }) => (
-        //             <div key={value} className={`${activeTab === value ? '' : 'hidden'}`}>
-        //                 {desc}
-        //             </div>
-        //         ))}
-        //     </div> */}
-        //     <TabsBody>
-        //         {data.map(({ value, desc }) => (
-        //         <TabPanel key={value} value={value}>
-        //             {desc}
-        //         </TabPanel>
-        //         ))}
-        //     </TabsBody>
-        // </Tabs>
     )
 }
 
