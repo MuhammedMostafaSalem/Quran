367c767d45d206267046e4b12932818098de056c
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2024-03-30 16:44:53 +0200

    Add language prop in getAllLive action

--- src/store/Actions/livTvAction.js
+++ src/store/Actions/livTvAction.js
@@ -1,9 +1,9 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import useGetData from "../../hook/useGetData";
 
-export const getAllLive = createAsyncThunk('livTv/getAllLive', async () => {
+export const getAllLive = createAsyncThunk('livTv/getAllLive', async ({language}) => {
     try {
-        const response = await useGetData(`https://mp3quran.net/api/v3/live-tv?language=ar`);
+        const response = await useGetData(`https://mp3quran.net/api/v3/live-tv?language=${language}`);
         return response.data;
     } catch (error) {
         console.log(error)
