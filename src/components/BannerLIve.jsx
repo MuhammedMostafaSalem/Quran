5a74e3476cf73090afcabece51618fe3174a1805
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2024-03-30 16:49:06 +0200

    Add useTranslation

--- src/components/BannerLIve.jsx
+++ src/components/BannerLIve.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { useTranslation } from 'react-i18next';
 
 const BannerLIve = () => {
+    const { t } = useTranslation();
+
     return (
         <div className="bannerLive relative top-[70px] sm:top-[60px] md:top-[70px]">
             <div className="banner-text">
-                <h6 className='text-[30px] sm:text-[35px] md:text-[48px] font-[700] font-[cursive] tracking-[2.5px]'>Live</h6>
+                <h6 className='text-[30px] sm:text-[35px] md:text-[48px] font-[700] font-[cursive] tracking-[2.5px]'>{t('Live')}</h6>
             </div>
         </div>
     )
