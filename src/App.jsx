13e790505389593c5b44b67f0d7d3c104c27e56b
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2024-03-30 16:50:23 +0200

    Add useTranslation, useSelector, useState and handleChangeLang

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,32 @@ import Live from "./pages/Live";
 import Surah from "./pages/Surah"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Tafsir from "./pages/Tafsir";
+import { useDispatch, useSelector } from "react-redux";
+import { useState } from "react";
+import { useTranslation } from "react-i18next";
+import { changeLanguage, setDirection } from "./store/Reducers/languageReducer";
 
 function App() {
+  const dispatch = useDispatch();
+  const { i18n } = useTranslation();
+  const { language, dir } = useSelector(state => state.lang);
+  const [langs, setLangs] = useState(language);
+  const [dirs, setDirs] = useState(dir);
+
+  const handleChangeLang = (lang) => {
+    i18n.changeLanguage(lang)
+    dispatch(changeLanguage(lang))
+    setLangs(lang)
+    
+    const newDir = lang === 'eng' ? 'ltr' : 'rtl';
+    dispatch(setDirection(newDir));
+    setDirs(newDir);
+  }
+  document.dir = dirs;
+
   return (
     <BrowserRouter>
-      <Header />
+      <Header langs={langs} handleChangeLang={handleChangeLang} />
       <Routes>
         <Route index element={<Surah />} />
         <Route path="/live" element={<Live />} />
