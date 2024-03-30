51e8d7d0c1905fef0e8e1d17fe420affda2cf856
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2024-03-30 16:47:17 +0200

    Add language prop in custom hook (useGetMoshaf)

--- src/hooks/useGetMoshaf.js
+++ src/hooks/useGetMoshaf.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllReciters, getAllSuwar } from "../store/Actions/recitersAction";
 
-const useGetMoshaf = () => {
+const useGetMoshaf = (language) => {
     const dispatch = useDispatch();
     const [selectedOption, setSelectedOption] = useState('');
     const [selectedSurah, setSelectedSurah] = useState('');
@@ -18,14 +18,16 @@ const useGetMoshaf = () => {
     } catch(err) {}
 
     useEffect(() => {
-        dispatch(getAllReciters({reciter: selectedOption}))
-    }, [selectedOption]);
+        if(reciters) {
+            dispatch(getAllReciters({language, reciter: selectedOption}))
+        }
+    }, [reciters, dispatch, selectedOption]);
 
     const handleSelectChange = (event) => {
         const selectedId = event.target.value;
 
         setSelectedOption(selectedId);
-        dispatch(getAllReciters({reciter: selectedId}));
+        dispatch(getAllReciters({language, reciter: selectedId}));
     };
 
     let moshafData = []
@@ -52,8 +54,10 @@ const useGetMoshaf = () => {
     } catch(err) {}
     
     useEffect(() => {
-        dispatch(getAllSuwar())
-    }, []);
+        if(surah) {
+            dispatch(getAllSuwar({language}))
+        }
+    }, [surah, dispatch]);
 
     let server = moshafData[0]?.server
 
