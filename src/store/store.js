4ca0226e7e28ff4bc01a21ac4c0d2dfc7732478c
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2024-03-30 16:45:12 +0200

    Add lang

--- src/store/store.js
+++ src/store/store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import recitersReducer from "./Reducers/recitersReducer";
 import livTvReducer from "./Reducers/livTvReducer";
 import tafasirReducer from "./Reducers/tafasirReducer";
+import languageReducer from "./Reducers/languageReducer";
 
 export const store = configureStore({
     reducer: {
         reciters: recitersReducer,
         livTv: livTvReducer,
-        tafsir: tafasirReducer
+        tafsir: tafasirReducer,
+        lang: languageReducer
     },
     devTools: true,
 })
