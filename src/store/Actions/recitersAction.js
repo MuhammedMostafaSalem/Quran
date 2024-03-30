99cbe4fa7877358de118c1ee7a1c7c9686dd8b35
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2024-03-30 16:44:34 +0200

    Add language prop in getAllReciters and getAllSuwar actions

--- src/store/Actions/recitersAction.js
+++ src/store/Actions/recitersAction.js
@@ -1,9 +1,9 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import useGetData from "../../hook/useGetData";
 
-export const getAllReciters = createAsyncThunk('reciters/getReciters', async ({reciter}) => {
+export const getAllReciters = createAsyncThunk('reciters/getReciters', async ({language, reciter}) => {
     try {
-        const response = await useGetData(`/api/v3/reciters?language=ar&reciter=${reciter}&rewaya=1`);
+        const response = await useGetData(`/api/v3/reciters?language=${language}&reciter=${reciter}&rewaya=1`);
         return response.data;
     } catch (error) {
         console.log(error)
@@ -11,9 +11,9 @@ export const getAllReciters = createAsyncThunk('reciters/getReciters', async ({r
     }
 });
 
-export const getAllSuwar = createAsyncThunk('reciters/getSuwar', async () => {
+export const getAllSuwar = createAsyncThunk('reciters/getSuwar', async ({language}) => {
     try {
-        const response = await useGetData(`https://mp3quran.net/api/v3/suwar?language=ar`);
+        const response = await useGetData(`https://mp3quran.net/api/v3/suwar?language=${language}`);
         return response.data;
     } catch (error) {
         console.log(error)
