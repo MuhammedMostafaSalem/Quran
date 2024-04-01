ad62ca8987f52ad769bdbf575da3fdf25076548c
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2024-04-01 13:39:06 +0200

    Add div element and wrap with Head component

--- src/pages/Tafsir.jsx
+++ src/pages/Tafsir.jsx
@@ -17,32 +17,34 @@ const Tafsir = () => {
     ] = useGetTafsir(language);
     
     return (
-        <Head title={t('Tafasir')} className='mt-[70px] sm:mt-[60px] md:mt-[70px]'>
-            <div className='container mx-auto py-[30px]'>
-                <h1 className="beautiful-title text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold text-center text-gray-400">{tafasirData.name}</h1>
-                <div className='w-full
-                    h-auto
-                    flex
-                    justify-center
-                    gap-4
-                    items-start
-                    flex-wrap
-                    overflow-y-auto
-                    mt-[30px]'
-                >
-                    {
-                        soarData.map((item, index) => (
-                            <TafsirList key={index} item={item} onClickSurah={onClickSurah} />
-                        ))
-                    }
+        <div className='mt-[70px] sm:mt-[60px] md:mt-[70px]'>
+            <Head title={t('Tafasir')}>
+                <div className='container mx-auto py-[30px]'>
+                    <h1 className="beautiful-title text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold text-center text-gray-400">{tafasirData.name}</h1>
+                    <div className='w-full
+                        h-auto
+                        flex
+                        justify-center
+                        gap-4
+                        items-start
+                        flex-wrap
+                        overflow-y-auto
+                        mt-[30px]'
+                    >
+                        {
+                            soarData.map((item, index) => (
+                                <TafsirList key={index} item={item} onClickSurah={onClickSurah} />
+                            ))
+                        }
+                    </div>
                 </div>
-            </div>
-            {
-                selectedSurah ?
-                    <Audio selectedSurah={selectedSurah} />
-                : null
-            }
-        </Head>
+                {
+                    selectedSurah ?
+                        <Audio selectedSurah={selectedSurah} />
+                    : null
+                }
+            </Head>
+        </div>
     )
 }
 
